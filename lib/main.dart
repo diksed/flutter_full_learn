4ec40b5c0f8542ce363894517c103ce99cdda48e
diksed <diksed00@gmail.com>
2022-08-22 20:37:20 +0300

    scaffold_learn

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_full_learn/101/container_sized_box_learn.dart';
+import 'package:flutter_full_learn/101/scaffold_learn.dart';
 import 'package:flutter_full_learn/101/text_learn_view.dart';
 
 void main() {
@@ -12,9 +13,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData.dark(),
-        home: ContainerSizedBoxLearn());
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: const ScaffoldLearnView(),
+    );
   }
 }
