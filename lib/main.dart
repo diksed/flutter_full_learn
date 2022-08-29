33da4e44e034985fc1074e4b88e624f9d71ae6c5
diksed <diksed00@gmail.com>
2022-08-29 15:41:40 +0300

    list_view_builder

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import '101/color_learn.dart';
-import '101/list_view_learn.dart';
+import '101/list_view_builder.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
               elevation: 0,
               backgroundColor: Colors.transparent,
               systemOverlayStyle: SystemUiOverlayStyle.light)),
-      home: const ListViewLearn(),
+      home: const ListViewBuilderLearn(),
     );
   }
 }
